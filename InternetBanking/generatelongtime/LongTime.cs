1982fd9e7052c7dd205c484ef0b6bf5a95cfda6d
Your Name <you@example.com>
2018-04-10 14:13:19 +0700

    Them package generatelongtime

---
+++ InternetBanking/generatelongtime/LongTime.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternetBanking.generatelongtime
+{
+    class LongTime
+    {
+        private readonly DateTime Jan1st1970 = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+        public long CurrentTimeMillis()
+        {
+            return (long)(DateTime.UtcNow - Jan1st1970).TotalMilliseconds;
+        }
+    }
+}
