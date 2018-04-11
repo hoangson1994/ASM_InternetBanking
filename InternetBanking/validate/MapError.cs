648c61fdda9d43dc6aa23862766e89859c087946
longstl <jonnykhanh1102@gmail.com>
2018-04-11 17:30:26 +0700

    change some text show error

--- InternetBanking/validate/MapError.cs
+++ InternetBanking/validate/MapError.cs
@@ -14,7 +14,7 @@ namespace InternetBanking
             ErrorEntity username = new ErrorEntity("Username can not empty", "Username not enough length (7 characters )", "Username incorrect character (a-z, A-Z, 0-9)");
             ErrorEntity password = new ErrorEntity("Password can not empty", "Password not enough length ( 7 characters )", "Password incorrect character (a-z, A-Z, 0-9)");
             ErrorEntity fullName = new ErrorEntity("Full Name can not empty", "Full Name not enough length ( 10 characters )", "Full Name incorrect character (a-z, A-Z)");
-            ErrorEntity birthday = new ErrorEntity("Birthday can not empty", "Birthday not enough lenghth (10 characters)", "Birthday incorrect character (01-01-2018)");
+            ErrorEntity birthday = new ErrorEntity("Birthday can not empty", "Do not enough age.", "Birthday incorrect character (yyyy/mm/dd)");
             ErrorEntity phone = new ErrorEntity("Phone can not empty", "Phone not enough length ( 10 characters)", "Phone incorect character (0-9)");
             ErrorEntity userId = new ErrorEntity("Identity Card can not empty", "Identity Card ( 11 characters)", "Identity Card incorrect character (0-9)");
             ErrorEntity email = new ErrorEntity("Email can not empty", "Email not enough length (10 characters)", "Email incorrect form (with @gmail.com or other)");
